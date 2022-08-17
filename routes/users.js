07c8bbdd8e466d1ec73f46308a5057d63e4579fe
zhangbo <1751179889@qq.com>
2022-08-17 15:05:13 +0800

    fix: 代码优化

--- routes/users.js
+++ routes/users.js
@@ -6,95 +6,104 @@ const { createToken, verifyToken } = require('../service/jwt');
 router.prefix('/user')
 
 router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+    ctx.body = 'this is a users response!'
 })
 
-function success(ctx, data = {}) {
-  ctx.body = {
-    message: '',
-    data,
-    code: 1
-  }
+/**
+ * 成功的返回
+ * @param {*} ctx
+ * @param {*} data 数据
+ */
+function success(ctx, data = {}, message = '') {
+    ctx.body = {
+        message,
+        data,
+        code: 1
+    }
 }
 
+/**
+ * 失败的返回
+ * @param {*} ctx
+ * @param {*} data 数据
+ */
 function fail(ctx, message = '') {
-  ctx.body = {
-    message,
-    data: null,
-    code: 0
-  }
+    ctx.body = {
+        message,
+        data: null,
+        code: 0
+    }
 }
 
 
 /**
  * created by zhangbo on 2022/08/16
  * 鉴权中间件函数
- * @returns 
+ * @returns
  */
 function auth() {
-  return (ctx, next) => {
-    const token = ctx.request.header.token 
-    console.log(token);
-    const isAuth = verifyToken(token);
-    if (isAuth) {
-        return next()
+    return (ctx, next) => {
+        const token = ctx.request.header.token
+        console.log(token);
+        const isAuth = verifyToken(token);
+        if (isAuth) {
+            return next()
+        }
+        return fail(ctx, '未登录')
     }
-    return fail(ctx, '未登录')
-  }
 }
 
 /**
- * ----------------------------------------------------------------------
+ * ---------------------------------------------------------------------- 接口示例
  */
-
 
 /**
  * 登陆
  */
- router.post('/login', async function (ctx, next) {
-  console.log(ctx.request.body);
-  let res = await User.findOne({
-    where: {
-
-    },
-    limit: 10,
-    offset: 0,
-  });
-
-  if (res) {
-    const data =  {
-      id: res.id,
-      username: res.username,
-      token: createToken({id: res.id})
+router.post('/login', async function (ctx, next) {
+    console.log(ctx.request.body);
+    const body =  ctx.request.body
+    const  param = {
+        userName: body.userName,
+        pwd: body.pwd
     }
-    success(ctx, data)
-  }
+    let res = await User.findOne({
+        where: param,
+    });
+    if (res) {
+        const data = {
+            id: res.id,
+            userName: res.userName,
+            token: createToken({id: res.id})
+        }
+        success(ctx, data)
+    } else {
+        fail(ctx, '用户名或密码错误')
+    }
 })
 
 /**
- * 登陆
+ * token测试
  */
- router.get('/tokenTest',auth(), async function (ctx, next) {
+router.get('/tokenTest', auth(), async function (ctx, next) {
     success(ctx, ctx.request.header)
 })
-
 
 
 /**
  * 列表
  */
 router.get('/getList', async function (ctx, next) {
-  let res = await User.findAndCountAll({
-    limit: 10,
-    offset: 0,
-  });
-  if (res) {
-    success(ctx, res)
-    // ctx.body = res;
-  }
-  else {
-    ctx.body = "没有数据";
-  }
+    let res = await User.findAndCountAll({
+        limit: 10,
+        offset: 0,
+        logging: true
+    });
+    if (res) {
+        success(ctx, res)
+    } else {
+        ctx.body = "没有数据";
+    }
 })
 
 
@@ -102,128 +111,145 @@ router.get('/getList', async function (ctx, next) {
  * 用户详情
  */
 router.get('/getUser', async function (ctx, next) {
-  console.log(ctx.request.query);
-  let id = ctx.request.query.id;
-  const res1 = await User.findOne({
-    where: {
-      id: id
+    console.log(ctx.request.query);
+    const id = ctx.request.query.id;
+    const res = await User.findOne({
+        where: {
+            id: id
+        }
+    });
+    if (!res) {
+        return fail(ctx, '该用户不存在')
     }
-  });
-  success(ctx, res1)
+    success(ctx, res)
 })
 
 
 /**
  * 删除
  */
-router.get('/delUser', async function (ctx, next) {
-  console.log(ctx.request.query);
-  let id = ctx.request.query.id;
-  let ids = id.split(',');
+router.post('/del', async function (ctx, next) {
+    console.log(ctx.request.body);
+    let id = ctx.request.body.id;
+    let ids = id.split(',');
 
-  const res1 = await userDb.destroy({
-    where: {
-      id: ids
+    const res = await User.destroy({
+        where: {
+            id: ids
+        }
+    })
+    if (res) {
+        success(ctx, res, '删除成功')
+    } else {
+        ctx.body = "没有数据";
     }
-  })
-  if (res1) {
-    ctx.body = { code: 0, data: res1, msg: "请求成功！" };
-  }
-  else {
-    ctx.body = "没有数据";
-  }
 })
 
 /**
  * 增加
  */
-router.get('/register', async function (ctx, next) {
-  let reqData = ctx.request.query;
-  let _params = {};
-  _params.admin_user = reqData.admin_user;
-  _params.admin_psd = reqData.admin_psd;
-  const res1 = await userDb.create(_params)
-  if (res1) {
-    ctx.body = { code: 0, data: res1, msg: "请求成功！" };
-  }
-  else {
-    ctx.body = "没有数据";
-  }
+router.post('/register', async function (ctx, next) {
+    let reqData = ctx.request.body;
+    const res = await User.create(reqData)
+    if (res) {
+        ctx.body = {code: 0, data: res, msg: "请求成功！"};
+    } else {
+        ctx.body = "没有数据";
+    }
 })
 
 /**
  * 更新
  */
-router.get('/update', async function (ctx, next) {
-  let reqData = ctx.request.query;
-  let id = reqData.id;
-  let row = await userDb.findAll({
-    where: {
-      id: id
+router.post('/update', async function (ctx, next) {
+    let reqData = ctx.request.body;
+    let id = reqData.id;
+    let row = await User.findone({
+        where: {
+            id: id
+        }
+    });
+    if (!row) {
+        return fail(ctx, '该用户不存在')
     }
-  });
-  let updateList = {};
-  for (let k in row[0].dataValues) {
-    updateList[k] = reqData[k]
-  }
-  console.log(updateList, "updateList-");
-  let result = await userDb.update(updateList, {
-    where: {
-      id: id
+    delete reqData.id
+    let result = await User.update(reqData, {
+        where: {
+            id: id
+        }
+    });
+    if (result) {
+        success(ctx, result, '更新成功')
+        // ctx.body = { code: 1, data: result, msg: "请求成功！" };
+    } else {
+        fail(ctx, '更新失败')
+        // ctx.body = { code: 0, data: result, errorMsg: "数据没有变化！" };
     }
-  });
-  if (result > 0) {
-    ctx.body = { code: 1, data: result, msg: "请求成功！" };
-  }
-  else {
-    ctx.body = { code: 0, data: result, errorMsg: "数据没有变化！" };
-  }
 })
 
 /**
  * 导出excel
  */
 router.get('/export', async function (ctx, next) {
-  const data = await User.findAll({
-    limit: 10,
-    offset: 0,
-    raw: true
-  });
-  const headerDis = []
-  const header = [
-    'nickname',
-    'id'
-  ]
-  /**
-   * json对象转sheet
-   */
-  const sheet = XLSX.utils.json_to_sheet([headerDis, ...data], {
-    header,
-    skipHeader: true,
-  });
-  /**
-   * 表格样式设置
-   */
-  sheet['!cols'] = Array.from({ length: 17 }, (v, i) => ({ wch: 18, size: 14 }));
-  /**
-   * 新建工作簿
-   */
-  const wb = XLSX.utils.book_new();
-  /**
-   * 向工作簿增加一个工作表(sheet)
-   * @param {wb} 工作簿对象
-   * @param {sheet} 工作表对象
-   * @param {name} 当前工作表名称
-   */
-  XLSX.utils.book_append_sheet(wb, sheet, `测试sheet`);
-  /**
-   * 工作簿生成文件
-   */
-  const excelBuffer =  XLSX.write(wb, {
-    type: 'buffer',
-  });
-  ctx.set('Content-Disposition', `attachment; filename=${encodeURIComponent('测试')}.xlsx`);
-  ctx.body = excelBuffer
+    // 查出数据
+    const data = await User.findAll({
+        attributes: [
+            'id',
+            'userName',
+            'openId',
+            'email',
+            'description',
+            'status'
+        ],
+        limit: 10,
+        offset: 0,
+        raw: true
+    });
+    // 表头中文
+    const headerDis = {id: '用户id', userName: '用户名', openId: 'openId', email: '邮箱', description: '描述',status: '状态'}
+    // 表头字段
+    const header = [
+        'id',
+        'userName',
+        'openId',
+        'email',
+        'description',
+        'status',
+    ]
+    /**
+     * json对象转sheet
+     */
+    const sheet = XLSX.utils.json_to_sheet([headerDis, ...data], {
+        header,
+        skipHeader: true,
+    });
+    /**
+     * 表格样式设置
+     */
+    sheet['!cols'] = Array.from({length: 17}, (v, i) => ({wch: 18, size: 14}));
+    /**
+     * 新建工作簿
+     */
+    const wb = XLSX.utils.book_new();
+    /**
+     * 向工作簿增加一个工作表(sheet)
+     * @param {wb} 工作簿对象
+     * @param {sheet} 工作表对象
+     * @param {name} 当前工作表名称
+     */
+    XLSX.utils.book_append_sheet(wb, sheet, `测试sheet`);
+    XLSX.utils.book_append_sheet(wb, sheet, `测试sheet2`);
+    /**
+     * 工作簿生成文件
+     */
+    const excelBuffer = XLSX.write(wb, {
+        type: 'buffer',
+    });
+    /**
+     * 设置下载头和文件名
+     */
+    ctx.set('Content-Disposition', `attachment; filename=${encodeURIComponent('测试')}.xlsx`);
+    ctx.body = excelBuffer
 })
 
 
--- sequelize/user.js
+++ sequelize/user.js
@@ -6,11 +6,11 @@ const db = require('./db');
  const dayjs = require('dayjs');
  const bcrypt = require('bcryptjs');
  const { Sequelize, Model } = require('sequelize');
- 
+
  const sequelize = require('./db');
- 
+
  const { Op } = Sequelize;
- 
+
  // 定义模型
  class User extends Model {
    static async findetail(uid) {
@@ -18,7 +18,7 @@ const db = require('./db');
        where: { id: uid },
      });
    }
- 
+
    static async getDetail(param, option) {
      const res = await this.findOne({
        where: param,
@@ -26,8 +26,8 @@ const db = require('./db');
      });
      return res;
    }
- 
- 
+
+
    static async findUserByName(username) {
      const user = await User.findOne({
        where: {
@@ -36,55 +36,55 @@ const db = require('./db');
      });
      return user;
    }
- 
+
    /**
     * @description:
     */
    static async findPage(req) {
      const where = {};
- 
+
      if (req.tel) {
        where.tel = req.tel;
      }
- 
+
      if (req.card) {
        where.card = {
          [Op.like]: `%${req.card}%`,
        };
      }
- 
+
      if (req.username) {
        where.username = {
          [Op.like]: `%${req.username}%`,
        };
      }
- 
+
      if (req.agentId) {
        where.agentId = req.agentId;
      }
- 
+
      if (req.source) {
        where.source = req.source;
      }
- 
+
      if (req.email) {
        where.email = {
          [Op.like]: `%${req.email}%`,
        };
      }
- 
+
      if (req.status) {
        where.status = req.status;
      }
- 
+
      if (req.platform) {
        where.platform = req.platform;
      }
- 
+
      if (typeof req.distributor === 'number') {
        where.distributor = req.distributor;
      }
- 
+
      if (req.created_at && Array.isArray(req.created_at) && req.created_at.length === 2) {
        let [startAt, endAt] = req.created_at;
        startAt = dayjs(startAt).startOf('day').format('YYYY-MM-DD HH:mm:ss');
@@ -93,7 +93,7 @@ const db = require('./db');
          [Op.between]: [startAt, endAt],
        };
      }
- 
+
      const findOption = {
        attributes: [
          'id',
@@ -125,15 +125,15 @@ const db = require('./db');
          },
        ],
      };
- 
+
      if (req.limit && req.page) {
        findOption.limit = req.limit;
        findOption.offset = (req.page - 1) * req.limit;
      }
- 
+
      return User.findAndCountAll(findOption);
    }
- 
+
    static async getSUMSector() {
      const param = {};
      return User.findOne({
@@ -141,7 +141,7 @@ const db = require('./db');
        where: param,
      });
    }
- 
+
    /**
     *统计总人数
     * @param status
@@ -161,7 +161,7 @@ const db = require('./db');
        where: param,
      });
    }
- 
+
    /**
     * 通过id获取用户信息
     * @param {*} id 用户id
@@ -175,7 +175,7 @@ const db = require('./db');
      });
      return result;
    }
- 
+
    /**
     * 更新用户
     * @param {Object} params
@@ -190,7 +190,7 @@ const db = require('./db');
      });
      return result;
    }
- 
+
    /**
     * id获取用户信息
     * @param {number} id
@@ -221,17 +221,13 @@ const db = require('./db');
      });
    }
  }
- 
+
  User.init(
    {
      id: {
        type: Sequelize.INTEGER,
        primaryKey: true, // 主键
        autoIncrement: true, // 主键自增长
-     },
-     nickname: {
-       type: Sequelize.STRING,
-       comment: '用户昵称',
      },
      email: {
        type: Sequelize.STRING,
@@ -242,29 +238,16 @@ const db = require('./db');
        unique: true,
        comment: '用户手机号',
      },
-     rate: {
-       type: Sequelize.DECIMAL(16, 2),
-       comment: '发币比例',
-     },
      pwd: {
        type: Sequelize.STRING,
        set(val) {
-         const salt = bcrypt.genSaltSync(12);
-         const pwd = bcrypt.hashSync(val, salt);
-         this.setDataValue('pwd', pwd);
+         // const salt = bcrypt.genSaltSync(12);
+         // const pwd = bcrypt.hashSync(val, salt);
+         // this.setDataValue('pwd', pwd);
        },
        comment: '登录密码',
      },
-     paypassword: {
-       type: Sequelize.STRING,
-       set(val) {
-         const salt = bcrypt.genSaltSync(10);
-         const pwd = bcrypt.hashSync(val, salt);
-         this.setDataValue('password', pwd);
-       },
-       comment: '支付密码',
-     },
-     openid: {
+     openId: {
        type: Sequelize.STRING,
        comment: '微信用户id',
      }, // 微信用户id
@@ -274,35 +257,7 @@ const db = require('./db');
        defaultValue: 1, // 1待审核 2审核通过 3审核失败 4停用
        comment: '用户状态',
      },
-     interest: {
-       type: Sequelize.DECIMAL(30, 18).UNSIGNED,
-       defaultValue: 0,
-       allowNull: false,
-       comment: '公司质押利息',
-     },
-    //  companyGas: {
-    //    type: Sequelize.DECIMAL(30, 18),
-    //    allowNull: false,
-    //    defaultValue: 0,
-    //    comment: '公司支付GAS费',
-    //  },
-     companyGasInterest: {
-       type: Sequelize.DECIMAL(30, 18).UNSIGNED,
-       allowNull: false,
-       defaultValue: 0,
-       comment: '公司支付GAS费利息',
-     },
-     sector: {
-       type: Sequelize.DECIMAL(16, 8),
-       defaultValue: 0,
-       comment: '当前存力/GB',
-     },
-     pledgeType: {
-       type: Sequelize.INTEGER,
-       defaultValue: 2,
-       comment: '1: 公司抵押 2 个人抵押',
-     }, // 用户唯一编码
-     username: {
+     userName: {
        type: Sequelize.STRING,
        comment: '用户名',
      },
@@ -310,118 +265,14 @@ const db = require('./db');
        type: Sequelize.STRING,
        comment: '描述',
      }, // 描述
-     card: {
-       type: Sequelize.STRING,
-       comment: '身份证号',
-     }, // 证件号码
-     profit: {
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0,
-       comment: '冻结收益',
-     },
-     borrow: {
-       type: Sequelize.DECIMAL(10, 4).UNSIGNED,
-       allowNull: false,
-       defaultValue: 0,
-       comment: '借贷金额',
-     },
-     borrowInterest: {
-       type: Sequelize.DECIMAL(10, 4).UNSIGNED,
-       allowNull: false,
-       defaultValue: 0,
-       comment: '借贷金额利息',
-     },
-     totalProfit: {
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0,
-       comment: '总收益',
-     },
-     withdrawal: {
-       // 自动提现标识
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0, // 1不自动提现 2自动提现
-       comment: '提现金额',
-     },
-     usdtWithdrawal: {
-       // 自动提现标识
-       type: Sequelize.DECIMAL(12, 4).UNSIGNED,
-       allowNull: false,
-       defaultValue: 0, // 1不自动提现 2自动提现
-       comment: 'USDT金额',
-     },
-     wallet: {
-       type: Sequelize.STRING,
-       unique: true,
-       comment: 'FIL 充值钱包地址',
-     },
-     usdtWallet: {
-       type: Sequelize.STRING,
-       unique: true,
-       comment: 'USDT 充值钱包地址',
-     },
-     freezePledge: {
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0,
-       comment: 'm12系列产品冻结金额',
-     },
-     agentId: {
-       type: Sequelize.INTEGER,
-       allowNull: false,
-       comment: '用户所属经销商',
-     },
-     source: {
-       type: Sequelize.INTEGER.UNSIGNED,
-       allowNull: false,
-       defaultValue: 1,
-       comment: '1. 国内 2.国外用户',
-     },
-     isDistributor: {
-       type: Sequelize.INTEGER,
-       comment: '1:普通用户，2:分销商',
-     },
-     distributor: {
-       type: Sequelize.INTEGER,
-       comment: '用户所属分销商',
-     },
-     miner: {
-       type: Sequelize.INTEGER,
-       allowNull: false,
-       defaultValue: 0,
-       comment: '0. 非独立节点用户 1.独立节点用户 2.独立接口 + 订单用户',
-     },
-     platform: {
-       type: Sequelize.INTEGER,
-       allowNull: false,
-       defaultValue: 1,
-       comment: '平台：1.DC 2.上轩',
-     },
-     country: {
-       type: Sequelize.STRING,
-       comment: '国家',
-     },
-     countryCode: {
-       type: Sequelize.STRING,
-       comment: '国家码',
-     },
-     migrateArrears: {
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0,
-       comment: '迁移费欠款，单位FIL',
-     },
-     migrateArrearsSupplement: {
-       type: Sequelize.DECIMAL(30, 18),
-       defaultValue: 0,
-       comment: '补缴迁移费欠款，单位FIL',
-     },
    },
    {
      sequelize,
-     tableName: 'user',
+     tableName: 'user_copy',
    },
  );
 
- 
+
  module.exports = {
    User,
  };
- 
